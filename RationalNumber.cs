13cb499cf56085f6eabbf6dc241a72ebbc901549
Ian24a <ianchristensen0@gmail.com>
2025-10-22 09:17:58 -0600

    not sure

--- RationalNumber.cs
+++ RationalNumber.cs
@@ -1,12 +1,19 @@
 ﻿public class RationalNumber : IEquatable<RationalNumber> , IComparable<RationalNumber>
 {
+    public int numerator;
+    public int denominator;
     public bool Equals(RationalNumber? other)
     {
+        if (other == numerator || denominator) return true;
+        if (other =! numerator || denominator) return false;
         throw new NotImplementedException();
     }
 
     public int CompareTo(RationalNumber? other)
     {
+        if (other / numerator || denominator== 1) return 1;
+        if (other / == -1) return -1;
+        if (other / == 0) return 0;
         throw new NotImplementedException();
     }
 }
